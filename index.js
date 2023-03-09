d72a1269ea4672de3ebd137b1aa99743a44d1973
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-03-09 09:17:40 -0700

    feat: Added wireframe images

--- index.js
+++ index.js
@@ -7,21 +7,6 @@ async function myFunc() {
   let category = 'animal';
   let response = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
   let parsedRes = await response.json();
+  console.log(parsedRes);
   div.innerHTML = parsedRes.value;
 }
-
-
-
-// // Single lin comment
-// let myString = "This is my string";
-
-// /* This is a 
-// multiline comment */
-
-// function myFunc () {
-//   console.log(myString);
-// }
-
-// myFunc();
-
-// console.log(`This is a string of text ${placeholder}`);
